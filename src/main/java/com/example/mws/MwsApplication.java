0d32b448453ab7e5f20568ac9add36d1ccb2029c
Jason <921341249@qq.com>
2023-03-29 15:39:49 +0800

    add

---
+++ src/main/java/com/example/mws/MwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwsApplication.class, args);
+    }
+
+}
